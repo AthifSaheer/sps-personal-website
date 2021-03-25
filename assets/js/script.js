809a0772ca9f1a9c173eb92d6526e958e88a66bd
athif <liteboook@gmail.com>
2021-03-25 08:51:56 +0530

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,3 +60,52 @@ function messageForm() {
     }
     // return false;
 }
+
+function btnSubmit() {
+    var fname = document.getElementsByName("firstname")[0].value;
+    var fnameWarngTxtLetter = document.getElementById("fname-warng-txt-letters")
+    var fnameWarngTxtNumber = document.getElementById("fname-warng-txt-number")
+    var fnameWarngTxtRequired = document.getElementById("fname-warng-txt-required")
+
+    var email = document.getElementsByName("email")[0].value;
+    var emailWarngTxt = document.getElementById("email-warng-txt");
+    var emailWarngRequired = document.getElementById("email-warng-txt-required");
+
+    var message = document.getElementsByName("message")[0].value;
+    var messageWarngTxt = document.getElementById("messg-wrng-txt")
+
+    if (fname.match(letters)){
+        fnameWarngTxtLetter.style.visibility = "hidden";
+        fnameWarngTxtNumber.style.visibility = "hidden";
+        fnameWarngTxtRequired.style.visibility = "hidden";
+    }else if(fname.match(numbers)) {
+        fnameWarngTxtLetter.style.visibility = "hidden";
+        fnameWarngTxtNumber.style.visibility = "visble";
+        fnameWarngTxtRequired.style.visibility = "hidden";
+    }else if(fname.match(letterNumber)) {
+        fnameWarngTxtLetter.style.visibility = "visible";
+        fnameWarngTxtNumber.style.visibility = "visible";
+        fnameWarngTxtRequired.style.visibility = "hidden";
+    }else if(fname == "") {
+        fnameWarngTxtRequired.style.visibility = "visible";
+        fnameWarngTxtLetter.style.visibility = "visible";
+        fnameWarngTxtNumber.style.visibility = "visible";
+    }
+
+    if(email.match(symbol)){
+        emailWarngTxt.style.visibility = "hidden";
+        emailWarngRequired.style.visibility = "hidden";
+    }else if(email.length == '') {
+        emailWarngTxt.style.visibility = "hidden";
+        emailWarngRequired.style.visibility = "visible";
+    }else if(!email.match(symbol)){
+        emailWarngTxt.style.visibility = "visible";
+        emailWarngRequired.style.visibility = "hidden";
+    }
+    
+    if(message.match(letters) || message.match(letterNumber) || message.match(numbers)) {
+        messageWarngTxt.style.visibility = "hidden";
+    }else if(message.length == '') {
+        messageWarngTxt.style.visibility = "visible";
+    }
+}
